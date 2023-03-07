17323174591258ad18ffe24d04832264720c9203
atwanzireT <tatwanzire@gmail.com>
2023-03-07 06:28:10 +0000

    bottom nav

--- app/src/main/java/com/example/farmer_discussion/homeActivity.java
+++ app/src/main/java/com/example/farmer_discussion/homeActivity.java
@@ -1,71 +1,96 @@
 package com.example.farmer_discussion;
 
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.example.farmer_discussion.databinding.ActivityHomeBinding;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 
 public class homeActivity extends AppCompatActivity {
+    ActivityHomeBinding binding;
+
     RecyclerView recyclerView;
     DatabaseReference database;
     IssueAdaptor issueAdaptor;
     ArrayList<IssueModal> list;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        recyclerView = findViewById(R.id.post_listRV);
-        database = FirebaseDatabase.getInstance().getReference("issues");
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-
-        list = new ArrayList<>();
-        issueAdaptor = new IssueAdaptor(this, list);
-        recyclerView.setAdapter(issueAdaptor);
-
-        database.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for( DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    IssueModal issueModal = dataSnapshot.getValue(IssueModal.class);
-                    list.add(issueModal);
-                }
-                issueAdaptor.notifyDataSetChanged();
+        replaceFragments(new homeFragment());
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        binding.bottonNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragments(new homeFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragments(new settingFragment());
+                    break;
+                case R.id.news:
+                    replaceFragments(new newsFragment());
+                    break;
+                case R.id.post:
+                    replaceFragments(new createIssueFragment());
+                    break;
+                case R.id.search:
+                    replaceFragments(new searchFragment());
+                    break;
             }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
+            return true;
         });
-        Button issueBtn = findViewById(R.id.addissueBtn);
-        issueBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent addIssue = new Intent(homeActivity.this, createIssueActivity.class);
-                startActivity(addIssue);
-            }
-        });
+
+
+//        recyclerView = findViewById(R.id.post_listRV);
+//        database = FirebaseDatabase.getInstance().getReference("issues");
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//
+//
+//        list = new ArrayList<>();
+//        issueAdaptor = new IssueAdaptor(this, list);
+//        recyclerView.setAdapter(issueAdaptor);
+//
+//        database.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for( DataSnapshot dataSnapshot : snapshot.getChildren()){
+//                    IssueModal issueModal = dataSnapshot.getValue(IssueModal.class);
+//                    list.add(issueModal);
+//                }
+//                issueAdaptor.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//        Button issueBtn = findViewById(R.id.addissueBtn);
+//        issueBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent addIssue = new Intent(homeActivity.this, createIssueActivity.class);
+//                startActivity(addIssue);
+//            }
+//        });
+    }
+    private void replaceFragments(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 
 //    public void addPostActivity(View view) {
